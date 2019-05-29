693f34b64984c30e7f64d10e79ef0c45c0b779d0
Damian Strycharczuk <8152038+Leny1996@users.noreply.github.com>
2019-05-29 23:47:17 +0200

    Fix #3

--- plants-backend/apiapp/models/models.py
+++ plants-backend/apiapp/models/models.py
@@ -4,34 +4,39 @@ from django.db.models.signals import post_save
 
 from django.dispatch import receiver
 
+
 class PlantationPreset(models.Model):
     id = models.AutoField(primary_key=True)
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     water_per_day = models.IntegerField(blank=True, null=True)
     how_many_times_to_water = models.IntegerField(blank=True, null=True)
-    expected_growth = models.IntegerField(blank=True,null=True)
+    expected_growth = models.IntegerField(blank=True, null=True)
     data_ins = models.DateField()
-    min_temp= models.FloatField(blank=True, null=True)
-    max_temp= models.FloatField(blank=True, null=True)
-    min_soil= models.FloatField(blank=True, null=True)
-    max_soil= models.FloatField(blank=True, null=True)
-    min_humidity= models.FloatField(blank=True, null=True)
-    max_humidity= models.FloatField(blank=True, null=True)
+    min_temp = models.FloatField(blank=True, null=True)
+    max_temp = models.FloatField(blank=True, null=True)
+    min_soil = models.FloatField(blank=True, null=True)
+    max_soil = models.FloatField(blank=True, null=True)
+    min_humidity = models.FloatField(blank=True, null=True)
+    max_humidity = models.FloatField(blank=True, null=True)
+
 
 class Plantation(models.Model):
     id = models.AutoField(primary_key=True)
-    id_preset=models.ForeignKey(PlantationPreset,on_delete=models.CASCADE,null=True)
+    id_preset = models.ForeignKey(
+        PlantationPreset, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=30)
+
 
 class PlantationMeasurements(models.Model):
 
     id = models.AutoField(primary_key=True)
-    id_plantation=models.ForeignKey(Plantation,on_delete=models.CASCADE)
-    temp= models.FloatField(blank=True,null=True)
+    id_plantation = models.ForeignKey(Plantation, on_delete=models.CASCADE)
+    temp = models.FloatField(blank=True, null=True)
     soil = models.FloatField(blank=True, null=True)
     humidity = models.FloatField(blank=True, null=True)
     data_ins = models.DateField()
+
 
 class PlantationAvg(models.Model):
 
@@ -46,15 +51,13 @@ class PlantationAvg(models.Model):
 class User2Plantation(models.Model):
 
     id = models.AutoField(primary_key=True)
-    id_plantation = models.ForeignKey(PlantationMeasurements, on_delete=models.CASCADE)
+    id_plantation = models.ForeignKey(Plantation, on_delete=models.CASCADE)
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Plantation2Arduino(models.Model):
 
     id = models.AutoField(primary_key=True)
-    id_plantation = models.ForeignKey(PlantationMeasurements, on_delete=models.CASCADE)
+    id_plantation = models.ForeignKey(Plantation, on_delete=models.CASCADE)
     id_arduino = models.IntegerField(null=False, blank=False)
     secret_code = models.CharField(max_length=32)
-
-
