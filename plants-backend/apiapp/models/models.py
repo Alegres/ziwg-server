71ad39df98b4e32e3bb9990b3c00ae7ae6747985
Damian Strycharczuk <8152038+Leny1996@users.noreply.github.com>
2019-06-10 11:25:50 +0200

    removed unused model field

--- plants-backend/apiapp/models/models.py
+++ plants-backend/apiapp/models/models.py
@@ -9,7 +9,6 @@ class PlantationPreset(models.Model):
     id = models.AutoField(primary_key=True)
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
-    water_per_day = models.IntegerField(blank=True, null=True)
     how_often_to_water = models.IntegerField(blank=True, null=True)
     expected_growth = models.IntegerField(blank=True, null=True)
     how_long_to_water = models.IntegerField(blank=True, null=True)
